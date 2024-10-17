40334439aa8741282f129884b2c10877345f0a25
Naga <naga@changecx.com>
2024-10-17 23:58:08 +0530

    Naga - Removed tests updated package versions

--- plugin/src/domain/bulkSync/services/LockService.ts
+++
@@ -1,5 +0,0 @@
-export interface LockService {
-    acquireLock(key: string): void;
-    releaseLock(key: string): void;
-    checkLock(key: string): void;
-}
--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,7 +1,6 @@
 import { pubsubAdapter } from './infrastructure/driving/adapter/eventSync/pubsubAdapter';
 import { GenericAdapter } from './infrastructure/driving/adapter/eventSync/genericAdapter';
 import * as dotenv from 'dotenv';
-import { bulkSyncApiAdapter } from './infrastructure/driving/adapter/bulkSync/bulkSyncApiAdapter';
 import { validateEnvironment } from './utils/validate-configurations';
 
 dotenv.config();
@@ -11,4 +10,3 @@ const main = (adapter: GenericAdapter) => {
     return adapter();
 };
 export const eventApp = main(pubsubAdapter);
-export const bulkSyncApp = main(bulkSyncApiAdapter);
